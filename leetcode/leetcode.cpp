d74d1fba061cf6afb9a7d972d7802d82635fd76a
Shaw <admin@shaw75.dev>
2024-09-14 10:02:09 +0800

    9.15

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -1,5 +1,6 @@
 ﻿#include <string>
 #include <deque>
+#include <vector>
 using namespace std;
 /*
 2390. 从字符串中移除星号
@@ -27,7 +28,6 @@ public:
         int len = s.length();
         std::deque<char> dq;
         std::string result;
-        result.p
         int i = 0;
         while(i<len){
             if (s[i] != '*') {
@@ -68,5 +68,62 @@ public:
             }
         }
         return st;
+    }
+};
+
+
+
+
+/*
+2848. 与车相交的点
+给你一个下标从 0 开始的二维整数数组 nums 表示汽车停放在数轴上的坐标。对于任意下标 i，nums[i] = [starti, endi] ，其中 starti 是第 i 辆车的起点，endi 是第 i 辆车的终点。
+返回数轴上被车任意部分覆盖的整数点的数目。
+
+示例 1：
+
+输入：nums = [[3,6],[1,5],[4,7]]
+输出：7
+解释：从 1 到 7 的所有点都至少与一辆车相交，因此答案为 7 。
+示例 2：
+
+输入：nums = [[1,3],[5,8]]
+输出：7
+解释：1、2、3、5、6、7、8 共计 7 个点满足至少与一辆车相交，因此答案为 7 。
+
+
+提示：
+
+1 <= nums.length <= 100
+nums[i].length == 2
+1 <= starti <= endi <= 100
+
+
+*/
+class Solution {
+public:
+    int numberOfPoints(vector<vector<int>>& nums) {
+        int C = 0;
+        for (const auto& interval : nums) {
+            C = max(C, interval[1]);  //得到J的最大值
+        }
+
+        vector<int> v(C + 1); //初始化j+1个空间的v  8
+        for (const auto& interval : nums) {
+
+
+            v[interval[0]] = max(v[interval[0]], interval[1]);    //v[3] =6  v[1] =5  v[4]=7
+        }
+        //v[1] =5 ,max=5 
+        int max = 0, cnt = 0;
+        for (int i = 1; i < C + 1; i++) {    //v[3] =6  v[1] =5  v[4]=7   v[2]=0 v[6]=0 v[7]=0 v[5]=0
+            if (v[i] > max) {
+                max = v[i];
+                cnt++;
+            }
+            else if (i <= max) {
+                cnt++;
+            }
+        }
+        return cnt;
     }
 };
