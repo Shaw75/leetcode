b75798f7ee8cc1d29df1472c2947c7aeccefaf1b
Shaw <admin@shaw75.dev>
2024-09-20 21:43:30 +0800

    9.20签到失败，DP困难题，超时

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -3,7 +3,9 @@
 #include <vector>
 #include <numeric>
 #include <unordered_map>
+#include <unordered_set>
 #include <queue>
+#include <set>
 using namespace std;
 /*
 2390. 从字符串中移除星号
@@ -302,5 +304,58 @@ public:
 
         }
         return b;
+    }
+};
+
+
+/*
+2376. 统计特殊整数
+提示
+如果一个正整数每一个数位都是 互不相同 的，我们称它是 特殊整数 。
+
+给你一个 正 整数 n ，请你返回区间 [1, n] 之间特殊整数的数目。
+
+
+示例 1：
+
+输入：n = 20
+输出：19
+解释：1 到 20 之间所有整数除了 11 以外都是特殊整数。所以总共有 19 个特殊整数。
+示例 2：
+
+输入：n = 5
+输出：5
+解释：1 到 5 所有整数都是特殊整数。
+示例 3：
+
+输入：n = 135
+输出：110
+解释：从 1 到 135 总共有 110 个整数是特殊整数。
+不特殊的部分数字为：22 ，114 和 131 。
+*/
+class Solution {
+public:
+    int countSpecialNumbers(int n) {
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            if (isSpecial(i)) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    bool isSpecial(int n) {
+        bool v[10] = { false };  
+        while (n > 0) {
+            int digit = n % 10;
+            if (v[digit]) {  
+                return false;
+            }
+            v[digit] = true;  
+            n /= 10;
+        }
+        return true;
     }
 };
