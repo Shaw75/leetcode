67ff7c6e61e7342c2392d1694923ea92eb54cbb6
Shaw <admin@shaw75.dev>
2024-09-22 17:21:50 +0800

    9.22签到

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -359,3 +359,33 @@ public:
         return true;
     }
 };
+
+
+
+/*997. 找到小镇的法官
+小镇里有 n 个人，按从 1 到 n 的顺序编号。传言称，这些人中有一个暗地里是小镇法官。
+
+如果小镇法官真的存在，那么：
+
+小镇法官不会信任任何人。
+每个人（除了小镇法官）都信任这位小镇法官。
+只有一个人同时满足属性 1 和属性 2 。
+给你一个数组 trust ，其中 trust[i] = [ai, bi] 表示编号为 ai 的人信任编号为 bi 的人。
+
+如果小镇法官存在并且可以确定他的身份，请返回该法官的编号；否则，返回 - 1 。*/
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        std::unordered_set<int> set;
+        int size = trust.size();
+        for (int i = 1; i < size; i++) {
+            if (trust[i - 1][0] != trust[i][0]) {
+
+
+            }
+
+        }
+        return trust[0][1];
+    }
+};
